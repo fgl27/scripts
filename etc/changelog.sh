7bf09b60ab5b81b75d123663a10d3bd478be75bd
Felipe Leon <fglfgl27@gmail.com>
2017-06-26 15:12:50 -0300

    up
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- etc/changelog.sh
+++ etc/changelog.sh
@@ -6,7 +6,7 @@
 # file.md can work with more data or have more lines then a page wiki
 
 # input variables set the below the rest must be automatic
-source_tree_c="hd/crn"; #path here must be inside your home directory
+source_tree_c="$HOME/hd/crn"; #path here must be inside your home directory
 source_tree_r="android/n"; #path here must be inside your home directory
 device_tree="device/motorola/quark/"; #path here must be inside of source tree
 kernel_tree="kernel/motorola/apq8084/"; #path here must be inside of source tree
