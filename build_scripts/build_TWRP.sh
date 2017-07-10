0504ae18a401a2351b3faea2cd27fe1a59ef5ad3
Felipe Leon <fglfgl27@gmail.com>
2017-07-10 09:58:07 -0300

    up
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build_scripts/build_TWRP.sh
+++ build_scripts/build_TWRP.sh
@@ -3,22 +3,40 @@
 #make -j4 multirom_zip
 #make -j4 multirom_uninstaller
 export TW_DEVICE_VERSION=MOD_1;
-om_folder="/media/bhb27/4508858d-ffa9-4a7f-a111-f598c9425438/om6/";
-cd $om_folder
-TWRP_V=$(grep '#define TW_MAIN_VERSION_STR       "' bootable/recovery/variables.h | head -1 | awk '{print $3}' | sed 's/\"//g');
-echo $TWRP_V;
 
-echo -e "\nCommit?\n 1 = Yes\n"
+echo -e "\Omni?\n 1 = Yes\n"
 read -r input1
 echo -e "\nYou choose: $input1"
 
 if [ "$input1" == "1" ]; then
-	cd build/
-	git fetch https://review.lineageos.org/LineageOS/android_build refs/changes/89/2689/1 && git cherry-pick FETCH_HEAD
-	cd -
+	om_folder="$HOME/hd/om6/";
+else
+	om_folder="$HOME/android/n/";
 fi
+cd $om_folder
+TWRP_V=$(grep '#define TW_MAIN_VERSION_STR       "' bootable/recovery/variables.h | head -1 | awk '{print $3}' | sed 's/\"//g');
+echo $TWRP_V;
+
+
+if [ "$input1" == "1" ]; then
+echo -e "\nCommit?\n 1 = Yes\n"
+read -r input2
+echo -e "\nYou choose: $input1"
+	if [ "$input2" == "1" ]; then
+		cd build/
+		git fetch https://review.lineageos.org/LineageOS/android_build refs/changes/89/2689/1 && git cherry-pick FETCH_HEAD
+		cd -
+	fi
 . build/envsetup.sh 
 lunch omni_quark-eng
+else
+	. build/envsetup.sh
+	1
+	1
+	0
+	lunch cm_quark-userdebug
+fi
 make clean
 time make recoveryimage -j8 2>&1 | tee twrp.txt
 cp -rf out/target/product/quark/recovery.img $HOME/android/twrp-$TWRP_V-$TW_DEVICE_VERSION-quark.img
+
