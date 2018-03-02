70934508fb6c551d374f09c626f92062cea26bfb
Felipe Leon <fglfgl27@gmail.com>
2018-03-02 09:06:53 -0300

    Up build_scripts/build_romrrn_fast.sh
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build_scripts/build_romrrn_fast.sh
+++ build_scripts/build_romrrn_fast.sh
@@ -28,27 +28,6 @@ if [ "$input1" == "1" ]; then
 	git fetch https://github.com/bhb27/frameworks_native/ oreo && git cherry-pick 01df205b39e2465a36deaf11f76f8a63da414c3d
 	cd - &> /dev/null || exit;
 
-#	echo -e "\\n	out Folder $folder"
-#	folder="packages/apps/Nfc/";
-#	echo -e "\\n	In Folder $folder \\n"
-
-#	cd $folder
-#	git fetch https://github.com/LineageOS/android_packages_apps_Nfc refs/changes/81/206081/1 && git cherry-pick FETCH_HEAD
-#	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
-	folder="system/nfc";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder
-	git fetch https://github.com/LineageOS/android_system_nfc refs/changes/69/206569/1 && git cherry-pick FETCH_HEAD
-	git fetch https://github.com/LineageOS/android_system_nfc refs/changes/85/206085/1 && git cherry-pick FETCH_HEAD
-	git fetch https://github.com/LineageOS/android_system_nfc refs/changes/86/206086/2 && git cherry-pick FETCH_HEAD
-	git fetch https://github.com/LineageOS/android_system_nfc refs/changes/87/206087/3 && git cherry-pick FETCH_HEAD
-	git fetch https://github.com/LineageOS/android_system_nfc refs/changes/88/206088/1 && git cherry-pick FETCH_HEAD
-	git fetch https://github.com/LineageOS/android_system_nfc refs/changes/89/206089/1 && git cherry-pick FETCH_HEAD
-	cd - &> /dev/null || exit;
-
 	echo -e "\\n	out Folder $folder"
 	folder="system/sepolicy/";
 	echo -e "\\n	In Folder $folder \\n"
