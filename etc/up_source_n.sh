406bffbff2fee4f1bb81f25f1d8f2541a5aabba6
Felipe Leon <fglfgl27@gmail.com>
2018-08-18 13:58:07 -0300

    Up chagelog
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- etc/up_source_n.sh
+++ etc/up_source_n.sh
@@ -69,7 +69,7 @@ up_s_checkout() {
 	done
 }
 
-cd "$HOME"/android/rr || exit;
+cd "$HOME"/android/sources || exit;
 
 sources_pull=("android_packages_services_Telephony" "android_packages_apps_Dialer" "android_packages_apps_Dialer" "frameworks_av" "android_packages_apps_Contacts" "android_packages_apps_ContactsCommon" "android_packages_apps_ExactCalculator" "android_packages_apps_PhoneCommon" "android_packages_apps_PackageInstaller" "art" "frameworks_native" "android_system_sepolicy" "android_packages_services_Telecomm" "android_packages_apps_Messaging" "system_core" "android_packages_apps_Nfc" "android_packages_apps_StorageManager");
 
