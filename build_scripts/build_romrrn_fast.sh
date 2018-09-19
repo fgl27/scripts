d902c7abb3b32660254139e59cc318d399bed034
Felipe Leon <fglfgl27@gmail.com>
2018-09-19 20:37:15 -0300

    Up build_scripts/build_romrrn_fast.sh
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build_scripts/build_romrrn_fast.sh
+++ build_scripts/build_romrrn_fast.sh
@@ -40,7 +40,7 @@ if [ "$input1" == "1" ]; then
 	cd $folder
 	git fetch https://github.com/bhb27/android_frameworks_base/ lineage-15.1 && git cherry-pick e26bc1c06abe7a382375a3e9ed3311ec4927d306
 	git fetch https://github.com/bhb27/android_frameworks_base/ lineage-15.1 && git cherry-pick add501e5811333e41fbb4436387a46bcec8132fe
-	git fetch https://github.com/bhb27/android_frameworks_base/ oreo && git cherry-pick 80f4c1e698989d29af045209444eb948f8ed1719^..08ee876d812d7799189ba5fc1f774832f5806aca
+	git fetch https://github.com/bhb27/android_frameworks_base/ oreo && git cherry-pick 80f4c1e698989d29af045209444eb948f8ed1719^..1436d02c6f95953d0231acfa8dbbaa992db193bb
 	cd - &> /dev/null || exit;
 
 	echo -e "\\n	out Folder $folder"
