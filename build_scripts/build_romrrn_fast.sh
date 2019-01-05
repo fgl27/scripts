586a74b8839f52b535946fe190391acbc778d596
Felipe Leon <fglfgl27@gmail.com>
2019-01-05 11:00:29 -0200

    Update build shs
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build_scripts/build_romrrn_fast.sh
+++ build_scripts/build_romrrn_fast.sh
@@ -23,9 +23,9 @@ read -r input3
 echo -e "\nYou choose: $input3"
 
 if [ "$input3" == "r" ]; then
-	cd $FOLDERR
+	cd $FOLDERR || exit
 elif [ "$input3" == "o" ]; then
-	cd $FOLDERL
+	cd $FOLDERL || exit
 fi
 
 echo -e "\nMake boot or a ROM?\n 1 = Boot\n"
@@ -37,7 +37,7 @@ if [ "$input1" == "1" ]; then
 	folder="frameworks/base/";
 	echo -e "\\n	In Folder $folder \\n"
 
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/fgl27/android_frameworks_base/ lineage-15.1 && git cherry-pick e26bc1c06abe7a382375a3e9ed3311ec4927d306
 	git fetch https://github.com/fgl27/android_frameworks_base/ lineage-15.1 && git cherry-pick add501e5811333e41fbb4436387a46bcec8132fe
 	git fetch https://github.com/fgl27/android_frameworks_base/ oreo && git cherry-pick 80f4c1e698989d29af045209444eb948f8ed1719^..ba9e0b2834026842b70f7f71eeb51607f4deebbc
@@ -48,7 +48,7 @@ if [ "$input1" == "1" ]; then
 	folder="packages/apps/Nfc";
 	echo -e "\\n	In Folder $folder \\n"
 
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/fgl27/android_packages_apps_Nfc/ cm-14.1 && git cherry-pick 8314ecd4ff33f8d51228314849b6b9f88fae34cd
 	cd - &> /dev/null || exit;
 
@@ -57,7 +57,7 @@ if [ "$input1" == "1" ]; then
 	folder="build/make/";
 	echo -e "\\n	In Folder $folder \\n"
 
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/crdroidandroid/android_build/ 8.1 && git cherry-pick 393ed84896908e5fba88247308e39185af937061
 	git fetch https://github.com/crdroidandroid/android_build/ 8.1 && git cherry-pick 34f73f7a6a7b03467c2ee673dd96f6ba592961d0
 	git fetch https://github.com/crdroidandroid/android_build/ 8.1 && git cherry-pick 771abdd472adee0f5674c861e2af6deda1bc1208
@@ -77,7 +77,7 @@ if [ "$input1" == "1" ]; then
 	folder="packages/apps/Settings";
 	echo -e "\\n	In Folder $folder \\n"
 
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ oreo && git cherry-pick fb694fb3e333d8c3ed366071635c6c93accd161c
 	git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ oreo && git cherry-pick c4782c842bb30eca97af9372ee8957b892a4f51e^..fbb37a1d071c07ec314dc740b34e555d02861b45
 	git fetch https://github.com/LineageOS/android_packages_apps_Settings refs/changes/26/229226/1 && git cherry-pick FETCH_HEAD
@@ -88,7 +88,7 @@ if [ "$input1" == "1" ]; then
 	folder="bionic";
 	echo -e "\\n	In Folder $folder \\n"
 
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/fgl27/android_bionic/ lineage-15.1 && git cherry-pick 279c7ed48cc03733553e7f4cef0735e4302a6b6e
 	git revert b0938317329c63e69352d96b8046f30f205b055b --no-edit
 	cd - &> /dev/null || exit;
@@ -97,7 +97,7 @@ if [ "$input1" == "1" ]; then
 
 	folder="system/extras/su";
 	echo -e "\\n	In Folder $folder \\n"
-	cd $folder
+	cd $folder || exit;
 	git revert bffcdefa59834186b75987541930dbfa92d15a21 --no-edit
 	git revert ae77c1a8aa19484d8d8196e55254f2c6f01d1aad --no-edit
 	cd - &> /dev/null || exit;
@@ -105,7 +105,7 @@ if [ "$input1" == "1" ]; then
 
 	folder="hardware/qcom/bt-caf";
 	echo -e "\\n	In Folder $folder \\n"
-	cd $folder
+	cd $folder || exit;
 	git revert ddaccd2176683b6de272e7d2718557dbe9b9fe1b --no-edit
 	cd - &> /dev/null || exit;
 	echo -e "\\n	out Folder $folder"
@@ -115,7 +115,7 @@ if [ "$input1" == "1" ]; then
 
 	folder="packages/apps/PackageInstaller";
 	echo -e "\\n	In Folder $folder \\n"
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/fgl27/android_packages_apps_PackageInstaller/ lineage-15.1 && git cherry-pick ff97dfb9005cf43a7f99d7e5697848849665f5a2^..1cfc30259de983c4af022d952734fc4987e1040f
 	cd - &> /dev/null || exit;
 	echo -e "\\n	out Folder $folder"
@@ -123,7 +123,7 @@ if [ "$input1" == "1" ]; then
 	folder="vendor/rr";
 	echo -e "\\n	In Folder $folder \\n"
 
-	cd $folder
+	cd $folder || exit;
 	git fetch https://github.com/fgl27/android_vendor_resurrection/ oreo && git cherry-pick 98e4dfcddab09148bc22062613e882f7157844c9
 	cd - &> /dev/null || exit;
 
@@ -135,7 +135,8 @@ export days_to_log=0
 export RR_BUILDTYPE="Mod"
 export WITH_ROOT_METHOD="rootless"
 export WITH_SU=true
-export ROM_VVV=$(grep PRODUCT_VERSION vendor/rr/config/common.mk | head -1 | awk '{print $3}');
+ROM_VVV=$(grep PRODUCT_VERSION vendor/rr/config/common.mk | head -1 | awk '{print $3}');
+export ROM_VVV;
 . build/envsetup.sh
 if [ "$input2" == "1" ]; then
 	export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx11g"
@@ -160,7 +161,7 @@ END2="$(date)";
 END=$(date +%s.%N);
 echo -e "\nBuild start $START2";
 echo -e "Build end   $END2 \n";
-echo -e "\n${bldgrn}Total time elapsed: $(echo "($END - $START) / 60"|bc ):$(echo "(($END - $START) - (($END - $START) / 60) * 60)"|bc ) (minutes:seconds). \n";
+echo -e "\nTotal time elapsed: $(echo "($END - $START) / 60"|bc ):$(echo "(($END - $START) - (($END - $START) / 60) * 60)"|bc ) (minutes:seconds). \n";
 
 #sudo shutdown -h now;
 
---
+++ build_scripts/build_romrrn_fast_p.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# simple build sh ... alias br='/home/user/source_folder/device/motorola/quark/rootdir/etc/sbin/build.sh'
+
+#timer counter
+START=$(date +%s.%N);
+START2="$(date)";
+echo -e "\n build start $(date)\n";
+
+#source tree folder yours machine source folder
+FOLDER_SOURCE=~/android/P;
+
+echo -e "\nCommit?\n 1 = Yes\n"
+read -r input1
+echo -e "\nYou choose: $input1"
+
+echo -e "\nMake clean?\n 1 = Yes\n"
+read -r input2
+echo -e "\nYou choose: $input2"
+
+cd $FOLDER_SOURCE || exit;
+
+echo -e "\nMake boot or a ROM?\n 1 = Boot\n"
+read -r input3
+echo -e "\nYou choose: $input3"
+
+if [ "$input1" == "1" ]; then
+
+	folder="hardware/qcom/bt-caf/";
+	echo -e "\\n	In Folder $folder \\n"
+
+	cd $folder || exit;
+	git revert fa98f0564a17ba5a8e1defa17a2fc73bcfd8f3de --no-edit
+	cd - &> /dev/null || exit;
+
+	echo -e "\\n	out Folder $folder"
+
+	folder="hardware/qcom/display-caf/apq8084/";
+	echo -e "\\n	In Folder $folder \\n"
+
+	cd $folder || exit;
+	git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/98/233598/1 && git cherry-pick FETCH_HEAD
+	git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/00/233600/2 && git cherry-pick FETCH_HEAD
+	git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/04/233604/1 && git cherry-pick FETCH_HEAD
+	cd - &> /dev/null || exit;
+
+	echo -e "\\n	out Folder $folder"
+
+	folder="hardware/qcom/media-caf/apq8084/";
+	echo -e "\\n	In Folder $folder \\n"
+
+	cd $folder || exit;
+	git fetch https://github.com/LineageOS/android_hardware_qcom_media refs/changes/02/233602/2 && git cherry-pick FETCH_HEAD
+	cd - &> /dev/null || exit;
+
+	echo -e "\\n	out Folder $folder"
+
+fi
+
+export WITH_SU=true
+. build/envsetup.sh
+if [ "$input2" == "1" ]; then
+	export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx11g"
+	./prebuilts/sdk/tools/jack-admin kill-server
+	./prebuilts/sdk/tools/jack-admin start-server
+	make clean
+fi
+
+lunch lineage_quark-userdebug
+
+if [ "$input3" == "1" ]; then
+	time mka bootimage -j8 2>&1 | tee quark.txt
+else
+	time mka bacon -j8 2>&1 | tee quark.txt
+fi
+
+# final time display *cosmetic...
+END2="$(date)";
+END=$(date +%s.%N);
+echo -e "\nBuild start $START2";
+echo -e "Build end   $END2 \n";
+echo -e "\nTotal time elapsed: $(echo "($END - $START) / 60"|bc ):$(echo "(($END - $START) - (($END - $START) / 60) * 60)"|bc ) (minutes:seconds). \n";
+
+#sudo shutdown -h now;
+
