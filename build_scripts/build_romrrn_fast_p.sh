b44ee6f55af7b16472a1625d1f0a9b0baca6c8ab
Felipe Leon <fglfgl27@gmail.com>
2019-01-22 17:13:18 -0200

    Up build_scripts/build_romrrn_fast_p.sh
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build_scripts/build_romrrn_fast_p.sh
+++ build_scripts/build_romrrn_fast_p.sh
@@ -7,7 +7,9 @@ START2="$(date)";
 echo -e "\n build start $(date)\n";
 
 #source tree folder yours machine source folder
-FOLDER_SOURCE=~/android/P;
+#source tree folder yours machine source folder
+FOLDER_RR=~/android/rrp;
+FOLDER_L=~/android/P;
 
 echo -e "\nCommit?\n 1 = Yes\n"
 read -r input1
@@ -17,11 +19,21 @@ echo -e "\nMake clean?\n 1 = Yes\n"
 read -r input2
 echo -e "\nYou choose: $input2"
 
-cd $FOLDER_SOURCE || exit;
-
-echo -e "\nMake boot or a ROM?\n 1 = Boot\n"
+echo -e "\nr or o?\n"
 read -r input3
 echo -e "\nYou choose: $input3"
+
+echo -e "\nMake boot or a ROM?\n 1 = Boot\n"
+read -r input4
+echo -e "\nYou choose: $input4"
+
+if [ "$input3" == "r" ]; then
+	FOLDER_SOURCE=$FOLDER_RR;
+elif [ "$input3" == "o" ]; then
+	FOLDER_SOURCE=$FOLDER_L;
+fi
+
+cd $FOLDER_SOURCE || exit;
 
 if [ "$input1" == "1" ]; then
 
@@ -63,7 +75,28 @@ if [ "$input1" == "1" ]; then
 
 fi
 
+#Set Branch and update kernel and vendor before build
+cd kernel/motorola/apq8084/
+git checkout P
+git pull origin P
+cd - &> /dev/null || exit;
+
+cd vendor/motorola/
+git checkout P
+git pull origin P
+cd - &> /dev/null || exit;
+
 export WITH_SU=true
+
+if [ "$input3" == "r" ]; then
+	export days_to_log=0
+	#export RR_BUILDTYPE="Mod"
+	export WITH_ROOT_METHOD="rootless"
+	export WITH_SU=true
+	ROM_VVV=$(grep PRODUCT_VERSION vendor/rr/config/common.mk | head -1 | awk '{print $3}');
+	export ROM_VVV;
+fi
+
 . build/envsetup.sh
 if [ "$input2" == "1" ]; then
 	export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx11g"
@@ -72,9 +105,13 @@ if [ "$input2" == "1" ]; then
 	make clean
 fi
 
-lunch lineage_quark-userdebug
+if [ "$input3" == "r" ]; then
+	lunch rr_quark-userdebug
+elif [ "$input3" == "o" ]; then
+	lunch lineage_quark-userdebug
+fi
 
-if [ "$input3" == "1" ]; then
+if [ "$input4" == "1" ]; then
 	time mka bootimage -j8 2>&1 | tee quark.txt
 else
 	time mka bacon -j8 2>&1 | tee quark.txt
