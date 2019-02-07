0568ec7f07c0d3a0e16cf431329d2c7db8709cd0
Felipe Leon <fglfgl27@gmail.com>
2019-02-07 14:22:49 -0200

    Up build_scripts/build_romrrn_fast_p.sh
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build_scripts/build_romrrn_fast_p.sh
+++ build_scripts/build_romrrn_fast_p.sh
@@ -52,7 +52,8 @@ if [ "$input1" == "1" ]; then
 		echo -e "\\n	In Folder $folder \\n"
 
 		cd $folder || exit;
-		git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ pie && git cherry-pick bf08f7f70f6f26c2ef5ba7c6d41f87d046a2d0cb^..b3b7f578846aaed2c1bbf4d6d58284b8147ac73b
+		git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ pie && git cherry-pick bf08f7f70f6f26c2ef5ba7c6d41f87d046a2d0cb^..fca5654427efa9826284bc13a99d1e20cf9d0264
+		git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ pie && git cherry-pick b3b7f578846aaed2c1bbf4d6d58284b8147ac73b
 		cd - &> /dev/null || exit;
 
 		echo -e "\\n	out Folder $folder"
