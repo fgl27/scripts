f0b9e91fbb2d09ea20e6e0f506e775f00f151a4a
fgl27 <fglfgl27@gmail.com>
2020-01-13 00:23:13 -0300

    Up script
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- build_scripts/build_romrrn_fast_ql.sh
+++ build_scripts/build_romrrn_fast_ql.sh
@@ -7,7 +7,9 @@ START2="$(date)";
 echo -e "\n build start $(date)\n";
 
 #source tree folder yours machine source folder
-FOLDER_L=~/android/Q;
+#source tree folder yours machine source folder
+FOLDER_RR=~/android/rrp;
+FOLDER_L=~/android/P;
 
 echo -e "\nCommit?\n 1 = Yes\n"
 read -r input1
@@ -26,43 +28,6 @@ cd $FOLDER_L || exit;
 if [ "$input1" == "1" ]; then
 
 	# changes to make the ROM the way I prefer
-
-	folder="frameworks/base/";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder || exit;
-	git fetch https://github.com/fgl27/android_frameworks_base/ lineage-16.0_test && git cherry-pick 3a84966e0ed8cb89d6acd6dec68a891af1c80c8c^..75b0287b554b4f2bfe3da2c002d2cf2a6365e9c4
-	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
-
-	folder="packages/apps/Settings";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder || exit;
-	git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ pie_test && git cherry-pick bf08f7f70f6f26c2ef5ba7c6d41f87d046a2d0cb
-	git fetch https://github.com/fgl27/Resurrection_packages_apps_Settings/ pie_test && git cherry-pick b3b7f578846aaed2c1bbf4d6d58284b8147ac73b
-	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
-
-	folder="packages/apps/Updater";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder || exit;
-	git fetch https://github.com/fgl27/android_packages_apps_Updater/ lineage-16.0 && git cherry-pick 56ed02db138683364649de3b615685e4ee3abbe4
-	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
-
-	folder="build/make";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder || exit;
-	git fetch https://github.com/fgl27/android_build/ lineage-16.0_test && git cherry-pick 2dc00ac4cf33cbddf7d350cc21f5fb2a8fa2bfca^..fd3ed1155908eeb01dda6924ccf12308a89d3108
-	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
 
 	#Disable nfc by default
 	
@@ -86,32 +51,22 @@ if [ "$input1" == "1" ]; then
 
 	echo -e "\\n	out Folder $folder"
 
-	#prevent spam logs from wifi
-	folder="system/connectivity/wificond/";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder || exit;
-	git fetch https://github.com/fgl27/system_connectivity_wificond/ Pie && git cherry-pick f695a663f751814ab35e30791693d784649fad4e^..31b7bd81e031bbe9505c82bc15670e4281b00d34
-	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
-
-	#change rom type name
-	folder="vendor/lineage";
-	echo -e "\\n	In Folder $folder \\n"
-
-	cd $folder || exit;
-	git fetch https://github.com/fgl27/android_vendor_resurrection/ lineage-16.0_test  && git cherry-pick fc0d0a362c5b665896a75f539a66930cb276e885
-	cd - &> /dev/null || exit;
-
-	echo -e "\\n	out Folder $folder"
-
 	#Media updates
-	folder="hardware/qcom/media-caf/apq8084";
+	folder="hardware/qcom-caf/apq8084/media";
 	echo -e "\\n	In Folder $folder \\n"
 
 	cd $folder || exit;
 	git fetch https://github.com/fgl27/android_hardware_qcom_media lineage-16.0-caf-8084  && git cherry-pick 1b209940f1e72e247d1c627479e0b517aa62083f
+	cd - &> /dev/null || exit;
+
+	echo -e "\\n	out Folder $folder"
+
+	#Fix sepolicy
+	folder="device/qcom/sepolicy-legacy";
+	echo -e "\\n	In Folder $folder \\n"
+
+	cd $folder || exit;
+	git fetch "https://github.com/LineageOS/android_device_qcom_sepolicy" refs/changes/11/261511/2 && git cherry-pick FETCH_HEAD
 	cd - &> /dev/null || exit;
 
 	echo -e "\\n	out Folder $folder"
@@ -130,7 +85,7 @@ git pull origin P
 cd - &> /dev/null || exit;
 
 # build SU
-export WITH_SU=true
+#export WITH_SU=true
 
 # Start the build
 . build/envsetup.sh
