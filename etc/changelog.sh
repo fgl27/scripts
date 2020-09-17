4578d7e2263d8023d342506b730ef2a147eb9aa5
fgl27 <fglfgl27@gmail.com>
2020-09-16 23:21:08 -0300

    Improve etc/changelog.sh
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- etc/changelog.sh
+++ etc/changelog.sh
@@ -17,6 +17,7 @@ source_tree=$source_tree_lq
 source_name=$source_name_lq
 
 export Changelog=$source_tree/Changelog.md
+export temptitle=$source_tree/temp123title
 
 if [ -f $Changelog ];
 then
@@ -71,6 +72,8 @@ contains() {
 
 git_log_repo() {
     skip=0;
+    #use a temp file to check the value of printedtitle outside the forall loop
+    echo "0" > $temptitle;
     printedtitle=0;
 
     repo forall -pc 'git log --oneline --after='$1' --until='$2 | sed 's/^$/#EL /' | sed 's/^/* /' | sed 's/* #EL //' | sed 's/* //' | while read string; do
@@ -90,26 +93,36 @@ git_log_repo() {
                 temp_two="${temp_two//\`/%60}"
                 echo "* [$temp_one](https://github.com/search?q=${temp_two}&type=Commits)" >> $Changelog;
             elif [ "$project" == 1 ]; then
-                #skip specifc device folder even if they are part of the repo manifest, as they are sorted separated
+                #skip specific device folder even if they are part of the repo manifest, as they are sorted separated
                 if [[ $string == *"$device_tree"* ]] || [[ $string == *"$kernel_tree"* ]] || [[ $string == *"$vendor_tree"* ]]; then
                     skip=1;
                 else
                     if [ "$printedtitle" == 0 ]; then
+                        # a line divider between device and source changes
+                        if [ "$3" == 1 ]; then
+                            echo "***" >> $Changelog;
+                            echo >> $Changelog;
+                        fi;
+
                         echo "#### $source_name source changes of $Until_Date:" >>  $Changelog;
+                        echo >> $Changelog;
+                        echo "1" > $temptitle;
                         printedtitle=1;
                     fi;
                     echo "* $string" >> $Changelog;
                     skip=0;
                 fi;
             fi;
-        else
+        elif [ "$printedtitle" == 1 ] && [ "$skip" == 0 ]; then
             echo >> $Changelog;
         fi
     done
+    
+    printedtitle=$(<"$temptitle");
 
     if [ "$printedtitle" == 1 ]; then
         echo >> $Changelog;
-        echo "#### $source_name source changes of $Until_Date End." >>  $Changelog;
+        echo "#### $source_name source changes of $2 End." >>  $Changelog;
         echo >> $Changelog;
     fi;
 }
@@ -125,6 +138,7 @@ for i in $(seq $days_to_log); do
     kernel=$(cd $kernel_tree && git log --oneline --after=$After_Date --until=$Until_Date);
     vendor=$(cd $vendor_tree && git log --oneline --after=$After_Date --until=$Until_Date);
     source=$(repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date');
+    devicechanges=0;
 
     if [ -n "${device##+([:space:])}" ] || [ -n "${kernel##+([:space:])}" ] || [ -n "${vendor##+([:space:])}" ] || [ -n "${source##+([:space:])}" ]; then
         # Line with after --- until was too long for a small ListView
@@ -157,21 +171,21 @@ for i in $(seq $days_to_log); do
     if [ -n "${device##+([:space:])}" ] || [ -n "${kernel##+([:space:])}" ] || [ -n "${vendor##+([:space:])}" ]; then
         echo "#### Device specific Changes of $Until_Date End." >>  $Changelog;
         echo >> $Changelog;
-        echo "***" >> $Changelog;
-        echo >> $Changelog;
+        devicechanges=1;
     fi
 
     if [ -n "${source##+([:space:])}" ]; then
-        git_log_repo $After_Date $Until_Date
+        git_log_repo $After_Date $Until_Date $devicechanges
     fi
 
 done
 
 sed -i 's/* project /#### /g' $Changelog
-echo >> $Changelog;
 
 echo "### [This Changelog was generated automatically Click here to see how](https://github.com/bhb27/scripts/blob/master/etc/changelog.sh)"    >> $Changelog;
 
 echo -e "\nChangelog generated file in $Changelog\n"
+
+rm -rf "$temptitle"
 
 exit;
